768514dbc9bc967110b3fb105b26c9c687cd9c6c
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-01-07 12:20:43 +0300

    isim düzenlendi

--- src/Layouts/Header/Main.jsx
+++ src/Layouts/Header/Main.jsx
@@ -54,7 +54,7 @@ export default function HeaderMain() {
                 sx={{ width: 24, height: 24 }}
               />
               <Typography variant="h6" color="white">
-                Göktuğ
+                Goktug
               </Typography>
             </Stack>
           </Stack>
